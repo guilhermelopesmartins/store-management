3a89509a43d856677a1f0a2b0478cf417b30e5b6
Guilherme Lopes Martins <guilhermelopesmartins123@gmail.com>
2026-09-03 18:52:08 -0300

    test: update ControllerTestExtensions to simulate middleware-set CompanyId

--- tests/StoreManagement.UnitTests/Controllers/ControllerTestExtensions.cs
+++ tests/StoreManagement.UnitTests/Controllers/ControllerTestExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace StoreManagement.UnitTests.Controllers;
@@ -8,13 +7,12 @@ public static class ControllerTestExtensions
 {
     public static void SetCompanyIdClaim(this ControllerBase controller, Guid companyId)
     {
-        var claims = new List<Claim> { new("companyId", companyId.ToString()) };
-        var identity = new ClaimsIdentity(claims, "TestAuth");
-        var principal = new ClaimsPrincipal(identity);
+        var httpContext = new DefaultHttpContext();
+        httpContext.Items["CompanyId"] = companyId;
 
         controller.ControllerContext = new ControllerContext
         {
-            HttpContext = new DefaultHttpContext { User = principal }
+            HttpContext = httpContext
         };
     }
 }
